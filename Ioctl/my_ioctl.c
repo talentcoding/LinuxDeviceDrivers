04e34e4a3f49bc5ad670ab774b104c4d7e17e37d
talentcoding <talentcodings@gmail.com>
2026-01-24 03:22:37 +0530

    char diver Ioctl implementation

---
+++ Ioctl/my_ioctl.c
@@ -0,0 +1,152 @@
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include<linux/slab.h>                 //kmalloc()
+#include<linux/uaccess.h>              //copy_to/from_user()
+#include <linux/ioctl.h>
+#include <linux/err.h>
+ 
+ 
+#define WR_VALUE _IOW('s','a',int32_t*)
+#define RD_VALUE _IOR('s','b',int32_t*)
+ 
+int32_t value = 0;
+ 
+dev_t dev = 0;
+static struct class *my_dev_class;
+static struct cdev my_cdev;
+
+
+static int      __init my_driver_init(void);
+static void     __exit my_driver_exit(void);
+static int      my_open(struct inode *inode, struct file *file);
+static int      my_release(struct inode *inode, struct file *file);
+static ssize_t  my_read(struct file *filp, char __user *buf, size_t len,loff_t * off);
+static ssize_t  my_write(struct file *filp, const char *buf, size_t len, loff_t * off);
+static long     my_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
+
+
+static struct file_operations fops =
+{
+        .owner          = THIS_MODULE,
+        .read           = my_read,
+        .write          = my_write,
+        .open           = my_open,
+        .unlocked_ioctl = my_ioctl,
+        .release        = my_release,
+};
+
+
+static int my_open(struct inode *inode, struct file *file)
+{
+        pr_info(" my_open function Device File Opened...!!!\n");
+        return 0;
+}
+
+
+static int my_release(struct inode *inode, struct file *file)
+{
+        pr_info("my_release function Device File Closed...!!!\n");
+        return 0;
+}
+
+
+static ssize_t my_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
+{
+        pr_info("my_read function ---\n");
+        return 0;
+}
+
+
+static ssize_t my_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
+{
+        pr_info("my_write function ---\n");
+        return len;
+}
+
+
+static long my_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+{
+         switch(cmd) {
+                case WR_VALUE:
+                        if( copy_from_user(&value ,(int32_t*) arg, sizeof(value)) )
+                        {
+                                pr_err("Data Write : Err!\n");
+                        }
+                        pr_info("Value = %d\n", value);
+                        break;
+                case RD_VALUE:
+                        if( copy_to_user((int32_t*) arg, &value, sizeof(value)) )
+                        {
+                                pr_err("Data Read : Err!\n");
+                        }
+                        break;
+                default:
+                        pr_info("Default\n");
+                        break;
+        }
+        return 0;
+}
+ 
+
+static int __init my_driver_init(void)
+{
+        /*Allocating Major number*/
+        if((alloc_chrdev_region(&dev, 0, 1, "my_device")) <0){
+                pr_err("Cannot allocate major number\n");
+                return -1;
+        }
+        pr_info("Major = %d Minor = %d \n",MAJOR(dev), MINOR(dev));
+ 
+        /*Creating cdev structure*/
+        cdev_init(&my_cdev,&fops);
+ 
+        /*Adding character device to the system*/
+        if((cdev_add(&my_cdev,dev,1)) < 0){
+            pr_err("Cannot add the device to the system\n");
+            goto r_class;
+        }
+ 
+        /*Creating struct class*/
+        if(IS_ERR(my_dev_class = class_create("my_class"))){
+            pr_err("Cannot create the struct class\n");
+            goto r_class;
+        }
+ 
+        /*Creating device*/
+        if(IS_ERR(device_create(my_dev_class,NULL,dev,NULL,"my_device"))){
+            pr_err("Cannot create the Device 1\n");
+            goto r_device;
+        }
+        pr_info("Device Driver Insert...Done!!!\n");
+        return 0;
+ 
+r_device:
+        class_destroy(my_dev_class);
+r_class:
+        unregister_chrdev_region(dev,1);
+        return -1;
+}
+
+
+static void __exit my_driver_exit(void)
+{
+        device_destroy(my_dev_class,dev);
+        class_destroy(my_dev_class);
+        cdev_del(&my_cdev);
+        unregister_chrdev_region(dev, 1);
+        pr_info("Device Driver Remove...Done!!!\n");
+}
+ 
+module_init(my_driver_init);
+module_exit(my_driver_exit);
+ 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("santhosh");
+MODULE_DESCRIPTION("char driver with ioctl implementation");
+MODULE_VERSION("1.0");
